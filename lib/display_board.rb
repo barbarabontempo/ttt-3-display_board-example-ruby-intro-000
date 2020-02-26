3ad162931a8a3af3e30712ae2055fa9d97110cf8
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-26 01:33:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def display_board
+def display_board(the_ttt)
+  puts the_ttt
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -11,5 +12,7 @@ puts "Welcome to Tic Tac Toe"
 
 display_board
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-puts board[0]
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# puts board[0]
+my_variable = "BITCH WTF"
+display_board(my_variable)
